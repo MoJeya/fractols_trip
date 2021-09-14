66c875bd5f990ba5264be4b716132b8108150c54
Moghanavathanan Jeyavathanan <mjeyavat@1-E-9.42heilbronn.de>
2021-09-14 19:18:37 +0200

    Work in progress

--- fractols.c
+++ fractols.c
@@ -6,41 +6,81 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 19:45:02 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/09/09 19:33:08 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/09/14 19:18:07 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractols.h"
-#include <stdio.h>
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+/**
+ * TODO: set point und invers_points in einer function packen
+*/
+
+void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
 {
 	char *dst;
-
-	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
+	dst = data->addr + ((y * data->line_lenght) + (x * data->bits_per_pixel) / 8);
 	*(unsigned int *)dst = color;
 }
 
-int key_hook(int keycode, t_vars *my_struct)
+int	set_point(t_vars *data,int *x, int *y, int flag, int len)
 {
-	if(keycode == 2)
+	int cnt;
+
+	cnt = 0;
+	while (cnt < len)
 	{
-		printf("Hello from key_hook!\n");
-		mlx_loop(my_struct->mlx);
+		if (flag == 0)
+			(*x)++;
+		else if (flag == 1)
+			(*y)++;
+		my_mlx_pixel_put(data, (*x), (*y), 0x00FF0000);
+		cnt++;
 	}
 	return (0);
 }
 
+
+void invers_points(t_vars *data,int *x, int *y, int flag, int len)
+{
+	int cnt;
+
+	cnt = 0;
+	while (cnt > len)
+	{
+		if (flag == 0)
+			(*x)--;
+		else if (flag == 1)
+			(*y)--;
+		my_mlx_pixel_put(data, (*x), (*y), 0x00FF0000);
+		cnt--;
+	}
+}
+
+/**
+ * x und y werden als punkte Uebergeben
+ * und wareaend ein neuer punkt gezeichnet wird werd auch Automatisch eine
+ * neue Koordinate abgespeichert, durch die addressen von den variabeln x und y
+*/
+
 void start_fractols()
 {
-	t_data	data;
-	t_vars	my_struct;
+	t_vars my_data;
+	int x, y, cnt;
 
-	my_struct.mlx = mlx_init();
-	my_struct.win = mlx_new_window(my_struct.mlx, 1920, 1080, "Fractols_42");
-	data.img = mlx_new_image(my_struct.mlx, 1300, 1000);
-	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_lenght, &data.endian);
-	my_mlx_pixel_put(&data, 100, 100, 0x00FF0000);
-	mlx_key_hook(my_struct.win, key_hook, &my_struct);
-	mlx_loop(my_struct.mlx);
+	x = 1920/2;
+	y = 1080/2;
+	cnt = 0;
+	my_data.mlx = mlx_init();
+	my_data.win = mlx_new_window(my_data.mlx, 1920, 1080, "Fractols");
+	my_data.img = mlx_new_image(my_data.mlx, 1920, 1080);
+	my_data.addr = mlx_get_data_addr(my_data.img, &my_data.bits_per_pixel, &my_data.line_lenght, &my_data.endian);
+	set_point(&my_data, &x, &y, 0, 100);
+	set_point(&my_data, &x, &y, 1, 100);
+	invers_points(&my_data, &x, &y, 0, -100);
+	invers_points(&my_data, &x, &y, 1, -100);
+	//my_mlx_pixel_put(&my_data, x, y, 0x00FF0000);
+	//my_mlx_pixel_put(&my_data, x, y, 0x00FF0000);
+	mlx_put_image_to_window(my_data.mlx, my_data.win, my_data.img, 0, 0);
+	mlx_loop(my_data.mlx);
 }
--- fractols.h
+++ fractols.h
@@ -6,23 +6,22 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 19:45:27 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/09/09 19:14:28 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/09/14 15:19:43 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct s_vars {
-	void *mlx;
-	void *win;
-}	t_vars;
-
-typedef struct s_data {
+	void 	*mlx;
+	void 	*win;
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_lenght;
 	int		endian;
-}	t_data;
 
+}	t_vars;
 void start_fractols();
