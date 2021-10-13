7d3f1383671762c6166df551b00785bfbf3ef6a7
Moghanavathanan Jeyavathanan <mjeyavat@1-E-7.42heilbronn.de>
2021-10-13 15:17:17 +0200

    work in progress

--- fractols.c
+++ fractols.c
@@ -6,14 +6,15 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 19:45:02 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/10/11 19:01:41 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/10/13 15:16:26 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractols.h"
 /**
  * //TODO: set point und invers_points in einer function packen
- * TODO: fill square
+ * //TODO: fill square
+ * TODO: calculation has to be corrected
 */
 
 void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
@@ -28,15 +29,16 @@ void	my_mlx_pixel_put(t_vars *data, int x, int y, int color)
 int	z_loop(double x, double y)
 {
 	int		cnt;
-
-	x = (x*x) - (y*y) + x;
-	y = 2 * x * y + y;
+	double 	x_old;
+	double 	y_old;
 	cnt = 0;
 	while (cnt < 100)
 	{
-		x = (x*x) - (y*y) + x;
-		y = 2 * x * y + y;
-		if (abs((int)x + (int)y)>=2)
+		x_old = x;
+		y_old = y;
+		x = (x*x) - (y*y) + x_old;
+		y = 2 * x * y + y_old;
+		if (sqrt((x*x) + (y*y)) >= 2)
 			break;
 		cnt++;
 	}
@@ -53,23 +55,23 @@ void	fill_pane_w_pixle(double x, double y, t_vars *data)
 	double	x_p = -2;
 	double	y_p = -2;
 
-	max_x = 1920;
-	max_y = 1080;
+	max_x = (1920/2)+200;
+	max_y = (1080/2)+200;
 	while (y < max_y)
 	{
-		if (x >= max_x)
+		if (x == max_x)
 		{
-			x = 0;
+			x = (1920/2) - 200;
 			x_p = -2;
 		}
 		while (x < max_x)
 		{
-			if (z_loop(x_p, y_p) == 1)
-				my_mlx_pixel_put(data, x, y, 0x00FF0000);
-			x_p += 0.1;
+			if (z_loop(x_p, y_p) == 0)
+				my_mlx_pixel_put (data, x, y, 0x00FF0000);
+			x_p += 0.01;
 			x++;
 		}
-		y_p += 0.1;
+		y_p += 0.01;
 		y++;
 	}
 }
@@ -80,8 +82,8 @@ void	start_fractols()
 	int	x;
 	int	y;
 
-	x = 0;
-	y = 0;
+	x = (1920/2) - 200;
+	y = (1080/2) - 200;
 	my_data.mlx = mlx_init();
 	my_data.win = mlx_new_window(my_data.mlx, 1920, 1080, "Fractols");
 	my_data.img = mlx_new_image(my_data.mlx, 1920, 1080);
@@ -91,4 +93,5 @@ void	start_fractols()
 	fill_pane_w_pixle(x, y, &my_data);
 	mlx_put_image_to_window(my_data.mlx, my_data.win, my_data.img, 0, 0);
 	mlx_loop(my_data.mlx);
+
 }
