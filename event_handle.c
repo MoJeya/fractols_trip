f2032f23a8d07a20d93de2cd9827e3fd1bf39378
Moghanavathanan Jeyavathanan <mjeyavat@1-E-9.42heilbronn.de>
2021-10-22 20:58:37 +0200

    zoom function has been worked on, still a work in progress

--- event_handle.c
+++ event_handle.c
@@ -6,7 +6,7 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 19:03:41 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/10/22 19:17:41 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/10/22 20:56:34 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,29 @@
 
 void	set_mouse_pos(int x, int y, t_vars *frac)
 {
-	frac->m_pos.x = x;
-	frac->m_pos.y = y;
+	if (x == 200)
+		printf("x:%d\ny:%d\nx_max:%f\n", x, y, frac->x_max);
+	if (x > 200)
+		printf("x:%d\ny:%d\nx_max:%f\n", x, y, frac->x_max);
+	if (x < 200)
+		printf("x:%d\ny:%d\nx_max:%f\n", x, y, frac->x_max);
 }
-
 /*
  * Re: Digital zoom calculations
  * Vertical and horizontal image size (in pixel) should by divided by zoom factor. For example,
- * 2560x1920 (4.9MP) image after 2х digital zoom makes 1280x960 (1.23MP). Total Number of pixel should by divided
+ * 2560x1920 (4.9MP) image after 2х digital zoom makes 1280x960 (1.23MP).
+ * Total Number of pixel should by divided
  * by zoom factor square. For 2x zoom you should divide by 4, 3x zoom -divide by 9 etc.
 */
-
 void	ft_zoom(t_vars *frac)
 {
-	frac->x_max -= 0.1;
-	frac->y_max -= 0.1;
-	frac->x_min += 0.1;
-	frac->y_min += 0.1;
+	double zoom_factor;
+
+	zoom_factor = 0.05;
+	frac->x_max -= zoom_factor;
+	frac->y_max -= zoom_factor;
+	frac->x_min += zoom_factor;
+	frac->y_min += zoom_factor;
 	if (frac->x_max == 0 || frac-> x_min == 0)
 	{
 		frac->x_max = 0.0;
@@ -45,11 +51,14 @@ void	ft_zoom(t_vars *frac)
 
 void	ft_dezoom(t_vars *frac)
 {
-	frac->x_max += 0.1;
-	frac->y_max += 0.1;
-	frac->x_min -= 0.1;
-	frac->y_min -= 0.1;
-	if (frac->x_max == 0 || frac-> x_min == 0)
+	double zoom_factor;
+
+	zoom_factor = 0.05;
+	frac->x_max += zoom_factor;
+	frac->y_max += zoom_factor;
+	frac->x_min -= zoom_factor;
+	frac->y_min -= zoom_factor;
+	if (frac->x_max == 2 || frac-> x_min == -2)
 	{
 		frac->x_max = 0.0;
 		frac->y_max = 0.0;
