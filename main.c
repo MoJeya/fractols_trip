eeffe878f14cbd931dfccffeae3d5da237b0eab4
Moghanavathanan Jeyavathanan <mjeyavat@1-E-9.42heilbronn.de>
2021-11-02 14:39:26 +0100

    done :D

--- main.c
+++ main.c
@@ -6,25 +6,49 @@
 /*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 17:28:18 by mjeyavat          #+#    #+#             */
-/*   Updated: 2021/10/30 16:07:22 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2021/11/02 14:20:15 by mjeyavat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractols.h"
 
-void	load_info_to(t_vars *fr, int argc)
+static void	close_window(int num)
 {
-	if (argc > 2)
+	exit(num);
+}
+
+static void	load_info_to(t_vars *fr)
+{
+	fr->img = mlx_xpm_file_to_image(fr->mlx, "./image/man.xpm",
+			&fr->window_width_x, &fr->window_height_y);
+	load_img(fr);
+}
+
+static void	ft_set_picker(int argc, char *argv[], t_vars *fractol, int input)
+{
+	if (argc == 2)
 	{
-		fr->img = mlx_xpm_file_to_image(fr->mlx, "./image/man.xpm",
-				&fr->window_width_x, &fr->window_height_y);
-		load_img(fr);
+		input = ft_atoi(argv[1]);
+		if (input == 1)
+			fractol->mod = input;
+		else if (input == 2)
+		{
+			fractol->c = fractol->p[fractol->p_cnt];
+			fractol->mod = input;
+		}
+		else
+		{
+			signal(SIGALRM, close_window);
+			alarm(2);
+			load_info_to(fractol);
+		}
+		start_fractols(fractol);
 	}
-	else
+	else if (argc == 1)
 	{
-		fr->img = mlx_xpm_file_to_image(fr->mlx, "./image/man.xpm",
-				&fr->window_width_x, &fr->window_height_y);
-		load_img(fr);
+		signal(SIGALRM, close_window);
+		alarm(2);
+		load_info_to(fractol);
 	}
 }
 
@@ -36,20 +60,7 @@ int	main(int argc, char *argv[])
 	input = 0;
 	fractol = (t_vars *)malloc(sizeof(t_vars));
 	init_data(fractol);
-	if (argc == 2)
-	{
-		input = ft_atoi(argv[1]);
-		if (input == 1)
-			fractol->mod = input;
-		if (input == 2)
-		{
-			fractol->c = fractol->p[fractol->p_cnt];
-			fractol->mod = input;
-		}
-		start_fractols(fractol);
-	}
-	else if (argc == 1)
-		load_info_to(fractol, argc);
+	ft_set_picker(argc, argv, fractol, input);
 	set_hooks(fractol);
 	mlx_loop(fractol->mlx);
 	return (0);
